37f0f11555ceb45149fe4854aa6e7e59eda548e2
tushar0i <171576246+tushar0i@users.noreply.github.com>
2026-03-16 14:33:09 +0530

    url handling

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask , request 
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<div> this is the thing </div>"
+
+@app.route('/greet/<name>')
+def name(name):
+    return f'<div> hi {name} </div>'
+
+@app.route('/sum/<int:num1>/<int:num2>')
+def sum(num1,num2):
+    return f'<div> {num1} + {num2} = {num1 + num2} </div>'
+
+@app.route('/handel_url_param')
+def handel_param():
+    return str(request.args)
+
+@app.route('/handel_url_param2')
+def handel_param2():
+    email = request.args.get('email')
+    name = request.args['name']
+    return f'{name},{email}'
+
+# http://127.0.0.1:9892/handel_url_param2?name=1&email=something@one.com&soem=thing this will work 
+# http://127.0.0.1:9892/handel_url_param2?name=1  output will contian email:None,name:'i'
+# http://127.0.0.1:9892/handel_url_param2?email=something@one.com this will give bad key error 
+
+@app.route('/handel_url_param3')
+def handel_param3():
+    if 'email' in request.args.keys() and 'name' in request.args.keys():
+        email = request.args['email']
+        name = request.args['name']
+        return f'HI {name} your email is {email}'
+
+
+
+if __name__  == '__main__':
+    app.run(host='127.0.0.1',port=9892,debug=True)
